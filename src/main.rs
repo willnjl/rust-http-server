27000bab9362bef7bfcac91d905d63056f61226c
Will Leighton <will@fifteenten.co.uk>
2024-07-25 11:06:10 +0100

    replace axum with actix

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use axum::{routing::get, Router};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[tokio::main]
-async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, Will!" }));
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 3000))?
+    .run()
+    .await
 }
