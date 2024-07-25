48ae2be74ae315dc7290a1203a0b2e092df1b496
Will Leighton <will@fifteenten.co.uk>
2024-07-25 15:54:01 +0100

    add db migration to script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,26 +2,49 @@
 set -x
 set -eo pipefail
 
+
+
+
+
+# Define vars
 DB_USER=${POSTGRES_USER:=postgres}
-
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-
 DB_NAME="${POSTGRES_DB:=newsletter}"
-
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-docker run \
--e POSTGRES_USER=${DB_USER} \
--e POSTGRES_PASSWORD=${DB_PASSWORD} \
--e POSTGRES_DB=${DB_NAME} \
--p "${DB_PORT}":5432 \
--d postgres \
-postgres -N 1000
+
+# Prerequisite checks
+
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed."
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "Use:"
+    echo >&2 " cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+    echo >&2 "to install it."
+    exit 1
+fi
 
 
-# Keep pinging Postgres until it's ready to accept commands
+
+# Launch docker
+if [[ -z "${SKIP_DOCKER}" ]] then
+    docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+fi
+
+
 export PGPASSWORD="${DB_PASSWORD}"
 
+# Keep pinging Postgres until it's ready to accept commands
 until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     >&2 echo "Postgres is still unavailable - sleeping"
     sleep 1
@@ -31,4 +54,8 @@ done
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 
 
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
