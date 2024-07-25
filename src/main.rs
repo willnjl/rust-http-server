2b00c527b842e98071ad2337d0c43e04e80d872f
Will Leighton <will@fifteenten.co.uk>
2024-07-25 10:57:31 +0100

    update name

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use axum::{routing::get, Router};
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, bob!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, Will!" }));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
