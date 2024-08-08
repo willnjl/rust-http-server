37795f1f6e896eb20bc35eebac9a796ded30470e
Will Leighton <will@fifteenten.co.uk>
2024-08-08 22:03:26 +0100

    add tracing subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
-use env_logger::Env;
-use rust_server::{configuration::get_configuration, startup::run};
+use rust_server::{ configuration::get_configuration, startup::run };
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{ BunyanFormattingLayer, JsonStorageLayer };
+use tracing_subscriber::{ layer::SubscriberExt, EnvFilter, Registry };
+use tracing_log::LogTracer;
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    LogTracer::init().expect("Failed to set logger");
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        std::io::stdout
+    );
+
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    
+    set_global_default(subscriber).expect("Failed to set subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-
+    let connection_pool = PgPool::connect(&configuration.database.connection_string()).await.expect(
+        "Failed to connect to Postgres."
+    );
     run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
-use actix_web::{post, web, HttpResponse, Responder};
+use actix_web::{ post, web, HttpResponse, Responder };
 use chrono::Utc;
-use sqlx::PgPool;
+use sqlx::{ query, PgPool };
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -11,28 +12,49 @@ struct FormData {
 
 #[post("/subscriptions")]
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> impl Responder {
-    log::info!("Saving new subscriber details in the database...");
+    let request_id = Uuid::new_v4();
+
+    let request_span =
+        tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
+    );
+
+    let request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+
+    tracing::info!("request_id {} - Saving new subscriber details in the database", request_id);
 
     let result = sqlx::query!(
-        r#"
-        INSERT INTO subscriptions (id,email, name,subscribed_at)
-        VALUES ($1, $2 ,$3 ,$4)
-        "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now(),
-    )
-    .execute(db_pool.get_ref())
-    .await;
+            r#"
+                INSERT INTO subscriptions (id,email, name,subscribed_at)
+                VALUES ($1, $2 ,$3 ,$4)
+            "#,
+            Uuid::new_v4(),
+            form.email,
+            form.name,
+            Utc::now()
+        )
+        .execute(db_pool.get_ref())
+        .instrument(query_span).await;
 
     match result {
         Ok(_) => {
-            log::info!("Saving new subscriber details in the database...");
+            tracing::info!("request_id {} - New subscriber details have been saved", request_id);
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
